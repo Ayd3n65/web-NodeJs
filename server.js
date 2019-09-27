1a3cb7492ae99883c5c3553a49324fb706f67d78
= <kiryamayg@gmail.com>
2019-09-27 10:06:15 +0300

    third commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var favicon = require('serve-favicon');
-var path = require('path')
+var path = require('path');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var methodOverride = require('method-override');
@@ -8,10 +8,8 @@ var log = require('./libs/log')(module);
 var config = require('./libs/config');
 var ArticleModel = require('./libs/mongoose').ArticleModel; 
 var app = express(); 
-
 app.use( favicon(path.join(__dirname, 'public', 'favicon.ico')) );
 app.use(morgan('tiny'));
-
 app.use( bodyParser.json()); 
 app.use(bodyParser.urlencoded({     
   extended: true
@@ -19,15 +17,11 @@ app.use(bodyParser.urlencoded({
 
 app.use(methodOverride());
 app.use(express.static(path.join(__dirname, "public"))); 
-
 app.get('/api/articles', function(req, res) {
 	return ArticleModel.find(function (err, articles) {
-		if (!err) 
-		{
+		if (!err) {
 			return res.send(articles);
-		} 
-		else 
-		{
+		} else {
 			res.statusCode = 500;
 			log.error('Internal error(' + res.statusCode + '): ' + err.message);
 			return res.send({ error: 'Server error' });
@@ -44,23 +38,18 @@ app.post('/api/articles', function(req, res) {
 	
 	article.save(function (err) {
 		if (!err) 
-		{
+{
 			log.info("article created");
 			return res.send({
 				status: 'OK',
 				article:article
 			});
-		} 
-		else 
-		{
+		} else {
 			console.log(err);
-			if (err.name == 'ValidationError') 
-			{
+			if (err.name == 'ValidationError') {
 				res.statusCode = 400;
 				res.send({ error: 'Validation error' });
-			} 
-			else 
-			{
+			} else {
 				res.statusCode = 500;
 				res.send({ error: 'Server error' }); 
 			}
@@ -72,8 +61,7 @@ app.post('/api/articles', function(req, res) {
 app.get('/api/articles/:id', function(req, res) {
 	return ArticleModel.findById(req.params.id, function (err, article) 
 	{
-		if (!article) 
-		{
+		if (!article) {
 			res.statusCode = 404;
 			return res.send({ error: 'Not found' });
 		}
@@ -88,32 +76,24 @@ app.get('/api/articles/:id', function(req, res) {
 });
 
 app.put('/api/articles/:id', function (req, res){
-	return ArticleModel.findById(req.params.id, function (err, article) 
-	{
-		if (!article) 
-		{
+	return ArticleModel.findById(req.params.id, function (err, article) {
+		if (!article) {
 			res.statusCode = 404;
 			return res.send({ error: 'Not found' });
 		}
-		article.title = req.body.title;
-		article.description = req.body.description;
-		article.author = req.body.author;
-		article.images = req.body.images;
-		return article.save(function (err) 
-		{
+			article.title = req.body.title;
+			article.description = req.body.description;
+			article.author = req.body.author;
+			article.images = req.body.images;
+		return article.save(function (err) {
 			if (!err) {
 				log.info("article updated");
 				return res.send({ status: 'OK', article:article });
-			} 
-			else 
-			{ 
-				if(err.name == 'ValidationError')
-				{
+			} else { 
+				if(err.name == 'ValidationError') {
 					res.statusCode = 400;
 					res.send({ error: 'Validation error' });
-				} 
-				else 
-				{
+				} else {
 					res.statusCode = 500;
 					res.send({ error: 'Server error' });
 				}
@@ -125,20 +105,15 @@ app.put('/api/articles/:id', function (req, res){
 
 app.delete('/api/articles/:id', function (req, res){
 	return ArticleModel.findById(req.params.id, function (err, article) {
-		if(!article) 
-		{
+		if(!article) {
 			res.statusCode = 404;
 			return res.send({ error: 'Not found' });
 		}
-		return article.remove(function (err) 
-		{
-			if (!err) 
-			{
+		return article.remove(function (err) {
+			if (!err) {
 				log.info("article removed");
 				return res.send({ status: 'OK' });
-			} 
-			else 
-			{
+			} else {
 				res.statusCode = 500;
 				log.error('Internal error(' + res.statusCode + '): ' + err.message);
 				return res.send({ error: 'Server error' });
@@ -146,26 +121,21 @@ app.delete('/api/articles/:id', function (req, res){
 		});
 	});
 });
-
 app.get('/ErrorExample', function(req, res, next){
 	next(new Error('Random error!'));
 });
-
 app.get('/test', function (req, res) {
   res.send('GET request to the homepage');
 });
-
 app.get('/api', function (req, res) {
 	res.send('API is running');
 });
-
 app.use(function(req, res, next){
 	res.status(404);
 	log.debug('Not found URL: ' + req.url);
 	res.send({ error: 'Not found' });
 	return;
 });
- 
 app.use(function(err, req, res, next){ 
 	res.status(err.status || 500);
 	log.error('123');
@@ -173,7 +143,6 @@ app.use(function(err, req, res, next){
 	res.send({ error: err.message });
  	return;
 });
-
 app.listen(config.get('port'), function(){
 	log.info('Express server listening on port ' + config.get('port'));
 }); 
